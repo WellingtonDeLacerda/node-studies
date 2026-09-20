7727cd37c8e53083865cde2665c819ff1f32c226
WellingtonDeLacerda <Wellingtonlacerda1515@gmail.com>
2026-09-20 20:49:16 -0300

    renomeando projetos

--- exercicios/02-protocolo-http/index.js
+++ exercicios/02-protocolo-http/index.js
@@ -2,7 +2,6 @@ const http = require('http')
 
 http.createServer(function (req,res) {
     res.end('Hello, this is my website')
-    }).listen(3000)
+}).listen(3000)
 
 console.log("servidor ligado")
-
